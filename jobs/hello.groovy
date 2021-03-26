f7f94f6f41fa84d4bc4492a5c3441eb25bf71c2f
Nouman72884 <noumansatti7@gmail.com>
2021-03-26 07:43:43 +0500

    jenkins job

--- jobs/hello.groovy
+++ jobs/hello.groovy
@@ -4,21 +4,20 @@ pipelineJob(jobname) {
   logRotator {
       numToKeep(5)
   }
-  parameters {
-    gitParam('GIT_BRANCH_NAME') {
-        description 'The Git branch to checkout'
-        type 'BRANCH'
-        defaultValue 'origin/master'
-        sortMode 'ASCENDING_SMART'
-    }
-  }
+//   parameters {
+//     gitParam('GIT_BRANCH_NAME') {
+//         description 'The Git branch to checkout'
+//         type 'BRANCH'
+//         defaultValue 'origin/master'
+//         sortMode 'ASCENDING_SMART'
+//     }
+//   }
   definition {
     cpsScm {
       scm {
         git {
           remote {
                 url('https://github.com/Nouman72884/simple_java_app.git')
-                credentials('githiub-token')
             }
             branches('master')
         }
